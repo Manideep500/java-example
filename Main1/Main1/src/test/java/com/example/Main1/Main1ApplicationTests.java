2e2e26b45fe9a1d6499766251f368322807a0e10
Manideep500 <venkatamanideepreddy@gmail.com>
2025-01-26 16:10:03 +0530

    branch addedd

---
+++ Main1/Main1/src/test/java/com/example/Main1/Main1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Main1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Main1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
